b0acf5e2b3df5dcb35dd81bfe97b4c9c88dfaf15
JuanLinkout <juan.lucas.rossi@gmail.com>
2021-04-08 22:36:34 -0300

    feat: ensure SignupController returns 400 if no name provided

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,15 @@
+import { badRequest } from './../helpers/http-helpers';
+import { HttpRequest, HttpResponse } from './../protocols/http';
+import { Controller } from './../protocols/controller';
+
+export class SignupController implements Controller {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const requiredParams = ['name', 'email']
+
+        for (let param of requiredParams) {
+            if (!httpRequest.body[param]) {
+                return badRequest(new Error())
+            }
+        }
+    }
+}
