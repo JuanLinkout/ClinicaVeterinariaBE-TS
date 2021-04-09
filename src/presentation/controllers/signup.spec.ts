deb63813fdd52a7ae5740a8d4a57bbd897907362
JuanLinkout <juan.lucas.rossi@gmail.com>
2021-04-08 22:49:35 -0300

    feat: ensure SignupController should return invalidParamError if passwords doesn't match

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,3 +1,4 @@
+import { InvalidParamError } from './../errors/invalidParamError';
 import { badRequest } from './../helpers/http-helpers';
 import { HttpRequest, HttpResponse } from './../protocols/http';
 import { SignupController } from './signup';
@@ -65,5 +66,21 @@ describe("Signup Contoller", () => {
 
         const httpResponse = await sut.handle(httpRequest)
         expect(httpResponse).toEqual(badRequest(new Error()))
+    })
+
+    it("Should return custom error if param is invalid", async () => {
+        const sut = new SignupController();
+
+        const httpRequest = {
+            body: {
+                name: 'anyname',
+                email: 'anyemail@gmail.com',
+                password: 'anypassword',
+                passwordConfirmation: 'anypassword123'
+            }
+        }
+
+        const httpResponse = await sut.handle(httpRequest)
+        expect(httpResponse).toEqual(badRequest(new InvalidParamError('passwordConfirmation')))
     })
 })
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,15 +1,21 @@
+import { InvalidParamError } from './../errors/invalidParamError';
 import { badRequest } from './../helpers/http-helpers';
 import { HttpRequest, HttpResponse } from './../protocols/http';
 import { Controller } from './../protocols/controller';
 
 export class SignupController implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const { body } = httpRequest
         const requiredParams = ['name', 'email', 'password', 'passwordConfirmation']
 
         for (let param of requiredParams) {
             if (!httpRequest.body[param]) {
                 return badRequest(new Error())
             }
+        }
+
+        if (body.password !== body.passwordConfirmation) {
+            return badRequest(new InvalidParamError('passwordConfirmation'))
         }
     }
 }
